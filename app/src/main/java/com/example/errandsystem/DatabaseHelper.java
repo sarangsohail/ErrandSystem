a37d48381b13bbc69b3459a266f32805ed5f40ca
Sarang Sohail <sarangsohail@gmail.com>
2018-07-05 18:43:47 +0100

    login screen in progress

---
+++ app/src/main/java/com/example/errandsystem/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.errandsystem;
+
+class DatabaseHelper {
+}
---
+++ app/src/main/java/com/example/errandsystem/User.java
@@ -0,0 +1,19 @@
+package com.example.errandsystem;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class User extends AppCompatActivity {
+
+    private TextView textViewName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        textViewName = (TextView) findViewById(R.id.text1);
+        String nameFromIntent = getIntent().getStringExtra("EMAIL");
+        textViewName.setText("Welcome " + nameFromIntent);
+    }
+}
