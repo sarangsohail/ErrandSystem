2993cdf2ca586a505d8a85e6dd637981382043f8
Sarang Sohail <sarangsohail@gmail.com>
2018-07-05 19:10:22 +0100

    problems encountered with 'logging in functionality

--- app/src/main/java/com/example/errandsystem/LoginActivity.java
+++ app/src/main/java/com/example/errandsystem/LoginActivity.java
@@ -7,8 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.widget.NestedScrollView;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
